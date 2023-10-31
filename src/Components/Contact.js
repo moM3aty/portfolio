1ba7635f326444b9799ec4ed0a388f2abeffe0b7
Mohamed Abo-elmaaty <61113131+moM3aty@users.noreply.github.com>
2023-10-31 03:57:07 +0200

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         
-        <div className="container">
+        <div className="container section-bg">
             <div className="section-title">
             <h2>Contact</h2>
             </div>
